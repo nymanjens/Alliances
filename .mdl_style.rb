8404337e8a1f7b543afc15415f54170e37a1f878
Jens Nyman <nymanjens.nj@gmail.com>
2016-11-30 22:31:51 +0000

    Enabled some MDL rules

--- .mdl_style.rb
+++ .mdl_style.rb
@@ -7,8 +7,4 @@ rule 'ul-indent', :indent => 4
 # Permanently disabled rules
 exclude_rule 'no-inline-html' # Inline HTML
 exclude_rule 'single-h1' # Multiple top level headers in the same document
-
-# Disabled rules to be revisited
-exclude_rule 'MD034' # Bare URL used
-exclude_rule 'MD036' # Emphasis used instead of a header
-exclude_rule 'MD041' # First line in file should be a top level header
+exclude_rule 'no-emphasis-as-header' # Emphasis used instead of a header
