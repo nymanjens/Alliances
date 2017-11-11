8ee42a0008484ffd5b31fe24a04c7d54c5eb830c
Jens Nyman <nymanjens.nj@gmail.com>
2017-11-11 15:30:27 +0000

    Fix Markdown issue

--- .mdl_style.rb
+++ .mdl_style.rb
@@ -5,6 +5,8 @@ rule 'line-length', :line_length => 100
 rule 'ul-indent', :indent => 4
 
 # Permanently disabled rules
+exclude_rule 'list-indent' # Inconsistent indentation for list items at the same level
+exclude_rule 'ul-start-left' # Consider starting bulleted lists at the beginning of the line
 exclude_rule 'no-inline-html' # Inline HTML
 exclude_rule 'single-h1' # Multiple top level headers in the same document
 exclude_rule 'no-emphasis-as-header' # Emphasis used instead of a header
